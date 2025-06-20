1884860cebdc2fa15cf3545c44dc1bb1971d465b
Jenil Patel <jenilpatelmehsana@gmail.com>
2025-06-20 15:54:35 +0530

    spring boot project extraction

---
+++ notification-scaling/src/main/java/com/example/notification_scaling/NotificationScalingApplication.java
@@ -0,0 +1,13 @@
+package com.example.notification_scaling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationScalingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationScalingApplication.class, args);
+	}
+
+}
---
+++ notification-scaling/src/test/java/com/example/notification_scaling/NotificationScalingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notification_scaling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationScalingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
